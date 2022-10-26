3bed380f09582d1d498695932e9d03a687de6c4f
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-10-26 15:51:10 -0300

    Array no componente List

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,17 +1,25 @@
 import React from "react";
+import { isTemplateExpression } from "typescript";
 
 function Lista() {
+  const tarefas = [
+    { tarefa: "React", tempo: "02:00:00" },
+    { tarefa: "Javascript", tempo: "01:00:00" }, 
+    { tarefa: "Typescript", tempo: "03:00:00" }
+  ];
   return (
     <aside>
       <h2>Estudos do Dia</h2>
-      <li>
-        <h3>React</h3>
-        <span>02:00:00</span>
-      </li>
-      <li>
-        <h3>Javascript</h3>
-        <span>01:00:00</span>
-      </li>
+      <ul>
+        {tarefas.map((item, index) => {
+          return (
+            <li key={index}>
+              <h3>{item.tarefa}</h3>
+              <span>{item.tempo}</span>
+            </li>
+          );
+        })}
+      </ul>
     </aside>
   );
 }
