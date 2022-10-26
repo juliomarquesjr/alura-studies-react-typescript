d58664891cdbe15994ba301faf67e660e86dab3a
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-10-26 15:36:07 -0300

    Criando a lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Formulario from "./components/Formulario";
+import Lista from "./components/Lista";
 
 function App() {
   return (
-    <>
+    <React.Fragment>
       <Formulario />
-    </>
+      <Lista />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Lista() {
+  return (
+    <aside>
+      <h2>Estudos do Dia</h2>
+      <li>
+        <h3>React</h3>
+        <span>02:00:00</span>
+      </li>
+      <li>
+        <h3>Javascript</h3>
+        <span>01:00:00</span>
+      </li>
+    </aside>
+  );
+}
+export default Lista;
