027e0e8a4e4cbcd13c20f5eefb5cdec5a96f8610
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-11-01 16:11:11 -0300

    Implementando o contador

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -4,24 +4,26 @@ import Relogio from "./Relogio";
 import style from "./Cronometro.module.scss";
 import { tempoParaSegundos } from "../../common/utils/time";
 import { ITarefa } from "../../types/tarefas";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface Props {
-  selecionado: ITarefa | undefined
+  selecionado: ITarefa | undefined;
 }
 
-function Cronometro({selecionado} : Props) {
+function Cronometro({ selecionado }: Props) {
   const [tempo, setTempo] = useState<number>();
 
-  if(selecionado?.tempo){
-    setTempo(tempoParaSegundos(selecionado.tempo))
-  }
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  }, [selecionado]);
 
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o crônometro</p>
-      <div className={style.relogioWrapper} >
-        <Relogio />
+      <div className={style.relogioWrapper}>
+        <Relogio tempo={tempo}/>
       </div>
       <Botao>Começar!</Botao>
     </div>
